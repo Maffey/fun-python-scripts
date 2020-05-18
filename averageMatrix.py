c4a2369901675662ebadc36f082a8409f5aeab8a
maffey <matiusz98@gmail.com>
2020-05-18 17:23:23 +0200

    added functionality for weighted calculations. more info in the comments.

--- averageMatrix.py
+++ averageMatrix.py
@@ -7,6 +7,10 @@ Following lines are matrices separated by spaces, one line for each matrix.
 If the calculations succeed, script appends the result at the end of the file.
 
 To use the script in command line terminal, use `python averageMatrix.py path/to/file.txt`
+
+Important note: the code was originally programmed to work on standard image denoise (without weights).
+Currently, it's possible to use it with weights too, although code became a little bit of a mess and it needs some
+hard coded values, although it provides useful information to calculate it - most notably, weights.
 """
 
 import argparse
@@ -65,20 +69,23 @@ def extract_file_data(file_path):
         print("[-] Error! Incorrect matrix dimensions.")
 
 
-def calculate_average_matrix(dimensions, matrices):
+def calculate_average_matrix(dimensions, matrices, weights):
     """Does the most important work: takes all the matrices and creates an average of them."""
 
     average_matrix = []
+    size = dimensions[0] * dimensions[1]
     # For all the positions in the matrix, get sum of all the values at the same position
     # from all matrices and calculate their average.
-    for i in range(dimensions[0] * dimensions[1]):
+    for i in range(size):
         positional_sum = 0
-        for matrix in matrices:
-            positional_sum += matrix[i]
+        for index, matrix in enumerate(matrices):
+            positional_sum += matrix[i] * weights[index]
 
         # We round the number since for image denoise, we need integers.
         # It's important to note that rounding uses IEEE 754 standard.
-        positional_average = round(positional_sum / len(matrices))
+        # Important: the line below is susceptible to change in case of using it for weighted calculations.
+        # Change sum(weights) to "size" for non-weighted option.
+        positional_average = round(positional_sum / sum(weights))
         average_matrix.append(positional_average)
     return average_matrix
 
@@ -107,16 +114,18 @@ def print_matrix(dimensions, matrix):
 # Additional functions for images of different quality (weights applied).
 def calculate_standard_deviation(dimensions, matrix):
     """Calculates standard deviation for SINGLE matrix. It is later used to establish weight values for matrices."""
-    # TODO: test this function.
     size = dimensions[0] * dimensions[1]
     average = sum(matrix) / size
-    standard_deviation = sum([(element - average) ** 2 for element in matrix]) / size
+    print(f"MATRIX DATA:\n{matrix}")
+    print(f"AVERAGE VALUE: {average}")
+    standard_deviation = sum([(element - average) ** 2 for element in matrix]) / (size - 1)
     return standard_deviation
 
 
-def calculate_weights(dimensions, matrices):
-    # calc weights based on standard deviation of all matrices. used in averaging
-    pass
+def print_standard_deviations(dimensions, matrices):
+    """Prints standard deviation of given matrix."""
+    for matrix in matrices:
+        print(f"STANDARD_DEVIATION: {calculate_standard_deviation(dimensions, matrix)}\n")
 
 
 # Get the path of the file from the user's input.
@@ -126,6 +135,11 @@ matrices_data = extract_file_data(worked_file_path)
 # If no errors occurred (meaning there IS some matrices data),
 # proceed with the calculations, saving results and printing the matrix.
 if matrices_data:
-    result = calculate_average_matrix(matrices_data[0], matrices_data[1])
+    print_standard_deviations(matrices_data[0], matrices_data[1])  # Prints info used for weighted images.
+
+    # Hard-coded weights list for each matrix. Allows to easily incorporate weighted calculations.
+    weights_list = [1.043, 1.126, 1, 1.065]
+
+    result = calculate_average_matrix(matrices_data[0], matrices_data[1], weights_list)
     save_result(worked_file_path, result)
     print_matrix(matrices_data[0], result)
