11d84d775f90049b37246c86a340b05a81d48e66
Maffey <matiusz98@gmail.com>
2020-09-06 12:44:10 +0200

    implemented removing headers.

--- merge_excel_documents.py
+++ merge_excel_documents.py
@@ -14,15 +14,23 @@ for file in excels_list:
         continue
     wb = openpyxl.load_workbook(os.path.join(excels_dir, file))
     sheet = wb.active
-    # TODO: Remove redundant headers.
     for row in sheet:
         list_of_cells_in_row = []
         for cell in row:
             list_of_cells_in_row.append(cell.value)
         list_of_sheets_rows.append(list_of_cells_in_row)
 
-# Save the data to a new workbook.
+# Open a new workbook to save results into.
 new_wb = openpyxl.Workbook()
+new_sheet = new_wb.active
+
+# Before saving the data, remove repeated headers.
+headers = list_of_sheets_rows[0]
+list_of_sheets_rows = [row for row in list_of_sheets_rows if row != headers]
+list_of_sheets_rows.insert(0, headers)
+
 for row_of_data in list_of_sheets_rows:
-    new_wb.active.append(row_of_data)
+    new_sheet.append(row_of_data)
+
+# Save the data to a new workbook.
 new_wb.save(os.path.join(excels_dir, target_name))
